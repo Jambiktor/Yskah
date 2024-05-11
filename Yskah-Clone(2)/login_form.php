980fa14412127021633a48f67926a97d029af46d
Jambiktor <ndendi00@gmail.com>
2024-05-11 14:36:26 +0800

    gadify login
    reposnive login

--- Yskah-Clone(2)/login_form.php
+++ Yskah-Clone(2)/login_form.php
@@ -1,55 +1,37 @@
 <?php
-    include("head.php");
+include("head.php");
 ?>
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Yskah Creation - Home</title>
-    <link rel="stylesheet" href="css\login1.css" />
-  </head>
-  <body>
-    <div class="login_form">
-    <div class="logo">
-      <img src="img\LOGO.png" alt="">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Yskah Creation - Home</title>
+  <link rel="stylesheet" href="css\login.css" />
+</head>
+
+<body>
+  <div class="container-fluid d-flex align-items-center justify-content-center flex-column flex-lg-row">
+    <div id="logo" class="mb-3">
+      <img class="img-fluid" src="img\LOGOO.png" alt="">
     </div>
-    <div class="container m-0">
-      <div class="wrapper bg-white">
+    <div class=" m-0 d-flex align-items-center justify-content-center">
+      <div class="wrapper bg-white p-3 pt-4 pb-4 p-lg-5 ">
         <h2 class="mb-4">Log in</h2>
         <form action="login.php" method="post">
           <div class="form-floating mb-3">
-            <input
-              type="text"
-              class="form-control"
-              id="username"
-              name="username"
-              placeholder="password"
-              required
-            />
-            <label for="username" class="form-label text-secondary"
-              >Username</label
-            >
+            <input type="text" class="form-control" id="username" name="username" placeholder="password" required />
+            <label for="username" class="form-label text-secondary">Username</label>
           </div>
           <div class="form-floating mb-3">
-            <input
-              type="password"
-              class="form-control"
-              id="password"
-              name="password"
-              placeholder="password"
-              required
-            />
-            <label for="password" class="form-label text-secondary"
-              >Password</label
-            >
+            <input type="password" class="form-control" id="password" name="password" placeholder="password" required />
+            <label for="password" class="form-label text-secondary">Password</label>
           </div>
 
-          <div
-            class="d-flex flex-row justify-content-between align-items-center"
-          >
-            <div class="">
-              <button type="submit" class="btn">Sign in</button>
+          <div class="d-flex flex-column flex-lg-column justify-content-between align-items-center">
+            <div class="mb-3">
+              <button id="login-submit" type="submit" class="btn">Sign in</button>
             </div>
             <div class="">
               <h6 class="text-secondary">
@@ -61,7 +43,8 @@
         </form>
       </div>
     </div>
-    </div>
-    
-  </body>
+  </div>
+
+</body>
+
 </html>
